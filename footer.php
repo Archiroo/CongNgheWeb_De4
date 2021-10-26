52f28017a71663ad401bc443aaf8ed7d168b96af
Archiroo <newbieit.nb@gmail.com>
2021-10-26 17:24:48 +0700

    Sửa index, thêm link

---
+++ footer.php
@@ -0,0 +1,8 @@
+    <!-- JQUERY -->
+    <script src="js/jquery-3.6.0.min.js"></script>
+    <!-- BOOSTRAP -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <!-- SCRIPT -->
+    <script src="js/myscript.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,33 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    include('header-student.php');
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
-</head>
-
-<body>
-    <section class="home" id="home">
-        <h1>education from home</h1>
+?>
+    <div class="home">
+        <h1>Education Support</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum neque aliquid similique error velit placeat vitae accusantium est nam magnam?</p>
-        <a href="#"><button class="btn">get started</button></a>
-
-        <div class="shape"></div>
-
-    </section>
-
-   
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-
-    <script src="js/script.js"></script>
-
-</body>
-
-</html>
-</body>
-
-</html>
+        <a href="#">
+            <button class="btn btn-index">Get Started</button>
+        </a>
+    </div>
+<?php
+    include('footer.php');
+?>
