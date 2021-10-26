5c1d293a89cc6e110bc69defc25c98545e425ced
Archiroo <newbieit.nb@gmail.com>
2021-10-27 00:31:16 +0700

    Chưa xong dashboard giáo viên

--- giaovien/dashboard.php
+++ giaovien/dashboard.php
@@ -1,6 +1,7 @@
 <?php
-    include('../header-teacher.php');
+    include('header-teacher.php');
 ?>
+<input type="checkbox" id = "sidebar-toggle">
 <div class="sidebar">
     <div class="sidebar-header">
         <h3 class="brand">
@@ -13,34 +14,49 @@
         <ul>
             <li>
                 <a href="">
+                    <i class="fas fa-home"></i>
                     <span>Home</span>
-                    <span></span>
                 </a>
             </li>
             <li>
                 <a href="">
+                    <i class="far fa-address-book"></i>
+                    <span>Subject</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fas fa-book"></i>
+                    <span>Homework</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="fas fa-user-friends"></i>
                     <span>Team</span>
-                    <span></span>
                 </a>
             </li>
             <li>
                 <a href="">
-                    <span></span>
-                    <span></span>
+                    <i class="fas fa-folder"></i>
+                    <span>Project</span>
                 </a>
             </li>
             <li>
                 <a href="">
-                    <span></span>
-                    <span></span>
+                    <i class="far fa-comments"></i>
+                    <span>Contact</span>
                 </a>
             </li>
             <li>
                 <a href="">
-                    <span></span>
-                    <span></span>
+                    <i class="fas fa-cog"></i>
+                    <span>Account</span>
                 </a>
             </li>
         </ul>
     </div>
 </div>
+<?php
+    include('../sinhvien/footer.php')
+?>
