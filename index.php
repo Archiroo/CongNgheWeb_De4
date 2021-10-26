9c0d135f85a3853b7f470cf906320acc43003383
Archiroo <newbieit.nb@gmail.com>
2021-10-26 21:18:06 +0700

    Thêm Login

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <?php
-    include('header-student.php');
+include('header-student.php');
 
 ?>
+<!-- CSS cùng với sinh viên -->
     <div class="home">
         <h1>Education Support</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum neque aliquid similique error velit placeat vitae accusantium est nam magnam?</p>
-        <a href="#">
+        <a href="#" id="login-btn">
             <button class="btn btn-index">Get Started</button>
         </a>
     </div>
+    <div class="login-form-container">
+        <div id="close-login-btn" class="fas fa-times"></div>
+        <form action="">
+            <h3>sign in</h3>
+            <span>User name</span>
+            <input type="email" name="" class="box" placeholder="Enter your email" id="">
+            <span>Pass word</span>
+            <input type="password" name="" class="box" placeholder="Enter your password" id="">
+            <div class="checkbox">
+                <input type="checkbox" name="" class="remember-me">
+                <label for="remember-me">Remember-me</label>
+            </div>
+            <input type="submit" value="Sign in" class="btn">
+            <p>For get password ? <a href="#">Click Here</a></p>
+            <p>Don't have an account ? <a href="#">Create One</a></p>
+        </form>
+    </div>
 <?php
-    include('footer.php');
+include('footer.php');
 ?>
