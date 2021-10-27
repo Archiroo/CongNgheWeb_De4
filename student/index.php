e3649d1aabbb4eb9acd9617a7a7d11ab52350c79
TrHoangLong <longtrinh29102001@gmail.com>
2021-10-28 04:22:34 +0700

    thêm mới giao diện sinh viên

---
+++ student/index.php
@@ -0,0 +1,69 @@
+<?php
+include('dashboard-student.php')
+?>
+<div class="main-content">
+    <header>
+        <div class="search-wrapper">
+            <input type="search" placeholder="Search">
+            <i class="ti-search"></i>
+        </div>
+        <div class="social-icons">
+            <span class="ti-bell"></span>
+            <span class="ti-comment"></span>
+            <div class="img-user"></div>
+        </div>
+    </header>
+    <main>
+        <h1>Over View</h1>
+        <div class="dash-card">
+            <div class="card-single">
+                <div class="card-body">
+                    <span class="fas fa-scroll"></span>
+                    <div>
+                        <h1>4</h1>
+                        <h1>Subject</h1>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <a href="">View all</a>
+                </div>
+            </div>
+            <div class="card-single">
+                <div class="card-body">
+                    <span class="fas fa-book"></span>
+                    <div>
+                        <h1>4</h1>
+                        <h1>Homework</h1>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <a href="">View all</a>
+                </div>
+            </div>
+            <div class="card-single">
+                <div class="card-body">
+                    <span class="fas fa-file-alt"></span>
+                    <div>
+                        <h1>4</h1>
+                        <h1>Project</h1>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <a href="">View all</a>
+                </div>
+            </div>
+            <div class="card-single">
+                <div class="card-body">
+                    <span class="fas fa-users"></span>
+                    <div>
+                        <h1>4</h1>
+                        <h1>Users</h1>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <a href="">View all</a>
+                </div>
+            </div>
+        </div>
+    </main>
+</div>
